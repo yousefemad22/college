0b84b83e1a41822834e498b12b3039db8a179328
yousef emad <yousef.emad199996@gmail.com>
2023-07-23 19:17:30 +0300

    Create header.php

---
+++ includes/template/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title;?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+   
+    <link rel="stylesheet" href="layout/css/<?php echo $css_file;?>">
+    <link rel="stylesheet" href="layout/css/toastr.min.css">
+    <script src="layout/js/jquery-3.5.1.min.js"></script>
+    <script src="layout/js/toastr.min.js"></script>
+</head>
+<body>
+
+
